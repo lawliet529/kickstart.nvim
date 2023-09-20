6617271032ea54e87b9900f06daa2ea90242cc85
lawliet529 <85656044+lawliet529@users.noreply.github.com>
2023-09-20 16:24:22 +0700

    Disable showmode

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,6 +2,9 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+vim.o.showmode = false
+
 return {
   'rlue/vim-barbaric',
 }
